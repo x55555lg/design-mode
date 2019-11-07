7df73aef99b69f02bd9f24937f53c16e103ac868
xuligang <429795986@qq.com>
2019-11-07 11:49:43 +0800

    degisn mode

--- src/test/java/com/lg/DesignModeApplicationTests.java
+++ src/test/java/com/lg/DesignModeApplicationTests.java
@@ -1,13 +1,19 @@
 package com.lg;
 
+import com.lg.ze_ren_lian.filter.ApplicationService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DesignModeApplicationTests {
 
+    @Autowired
+    private ApplicationService applicationService;
+
     @Test
     void contextLoads() {
+        applicationService.mockedClient();
     }
 
 }
